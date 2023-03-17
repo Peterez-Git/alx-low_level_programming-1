7eed0ae660e9f67f5f0c86a0b515c9d45c6c865e
oladapsy <dapoodedeyi@yahoo.com>
2023-03-16 21:28:06 -0700

    5-print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this fuction prints exactly
+ *
+ * Return: 0 whenn successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
