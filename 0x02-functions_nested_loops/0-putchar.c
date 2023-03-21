aeceaf780c731292df11810555f0e3a7726f1780
oladapsy <dapoodedeyi@yahoo.com>
2023-03-21 10:07:45 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "header.h"
-
+#include <main.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	write("_putchar\n");
-	return 0;
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+int _putchar(char c);
+void print-alphabet(void);
+void print_alhabet_x10(void);
