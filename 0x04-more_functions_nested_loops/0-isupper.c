16cfac1af6b2582d853459fba916f5ee298130f8
oladapsy <dapoodedeyi@yahoo.com>
2023-03-24 05:49:06 +0000

    yes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *  * _isupper - uppercase letters
- *   * @c: char to check
- *    *
- *     * Return: 0 or 1
- *      */
+ * _isupper - uppercase letters
+ *  @c: char to check
+ * Return: 0 or 1
+ **/
 
 int _isupper(int c)
 {
-		if (c >= 65 && c <=90)	
+	if (c >= 65 && c < 90)
+		{
 		return (1);
-		else
+		}
 		return (0);
 }
 
