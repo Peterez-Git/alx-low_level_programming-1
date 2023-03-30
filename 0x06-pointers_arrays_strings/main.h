3ec308a9fed9eaca2933b800706120a2ab3d124b
oladapsy <dapoodedeyi@yahoo.com>
2023-03-30 15:31:05 -0700

    another one

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,5 @@ void print_number(int n);
 int _putchar(char c);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif /*MAIN_H*/
