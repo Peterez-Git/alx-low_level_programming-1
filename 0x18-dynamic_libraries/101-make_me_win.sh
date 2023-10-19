b902912d3bd7334c74b0138b8ac667c808e85d37
oladapsy <dapoodedeyi@yahoo.com>
2023-10-18 23:06:51 -0700

    test

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/oladapsy/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/libhack.so"
+wget -P .. https://raw.githubusercontent.com/oladapsy/alx-low_level_programming/master/0x18-dynamic_libraries/liball.so
+export LD_PRELOAD="$PWD/liball.so"
